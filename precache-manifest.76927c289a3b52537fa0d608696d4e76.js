504557bb2b94a5116ea5cdc2effdb5de5efe48df
unknown <nuty-girl@mail.ru>
2019-12-17 17:01:09 +0200

    Updates

---
+++ precache-manifest.76927c289a3b52537fa0d608696d4e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1627dd8019faa0c464b35420c69eb11e",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "61bbf71c89fb23fcf205",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.b18376dc.chunk.css"
+  },
+  {
+    "revision": "1e8c732aea5b8ddfa4ff",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.ed2be112.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.ed2be112.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61bbf71c89fb23fcf205",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.30eea073.chunk.js"
+  },
+  {
+    "revision": "5898b43781fcd94828d3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.8d80df19.js"
+  }
+]);
